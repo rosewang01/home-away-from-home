4225746b2d608af8c59c5af29028bc4ad9ead201
Rose Wang <rosewang01@gmail.com>
2023-04-30 09:41:42 -0400

    finished zipcode routes

--- backend/src/routes/city.route.ts
+++ backend/src/routes/city.route.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import { getAllZipCodeDataByCity, getBestZipCodesByCost, getBestZipCodesByGrowth } from "../controllers/city.controller.js";
 
 const cityRouter = express.Router();
 
-cityRouter.get("/all", );
-cityRouter.get("/job/:job", );
-cityRouter.get("/employer/:employer", );
-cityRouter.get("/growth", );
-cityRouter.get("/debt", );
+cityRouter.get("/all", getAllZipCodeDataByCity);
+cityRouter.get("/growth", getBestZipCodesByGrowth);
+cityRouter.get("/cost", getBestZipCodesByCost);
 
 export { cityRouter };
