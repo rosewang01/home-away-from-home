b4a3b576b6a18caa719bfb4a2d7161c8a3436c58
Rose Wang <rosewang01@gmail.com>
2023-04-30 09:44:07 -0400

    rebase

---
+++ backend/src/models/h1b.model.ts
@@ -0,0 +1,15 @@
+interface IH1b {
+    case_id: number;
+    case_status: string;
+    emp_name: string;
+    job_title: string;
+    soc_code: string;
+    soc_name: string;
+    full_time_position: boolean;
+    prevailing_yearly_wage: number;
+    work_state: string;
+    work_city: string;
+
+}
+
+export default IH1b
