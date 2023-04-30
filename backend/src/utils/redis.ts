e52bbb18a76c86d583abda893b3452087ad9cfb7
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2023-04-30 10:58:49 -0400

    add flag to disable redis (#4)

--- backend/src/utils/redis.ts
+++ backend/src/utils/redis.ts
@@ -2,17 +2,26 @@ import { createClient } from "redis";
 
 const client = createClient();
 
-client.on("error", (error: string) => {
-  console.error(`redis client error: ${error}`);
-});
+if (process.env.REDIS_ENABLED !== "false") {
+  client.on("error", (error: string) => {
+    console.error(`redis client error: ${error}`);
+  });
 
-await client.connect();
+  await client.connect();
+}
+
 
 const redisGet = async (key: string): Promise<string | null> => {
+  if (process.env.REDIS_ENABLED === "false") {
+    return null;
+  }
   return await client.get(key);
 }
 
 const redisSet = async (key: string, value: string): Promise<void> => {
+  if (process.env.REDIS_ENABLED === "false") {
+    return;
+  }
   await client.set(key, value);
 }
 
