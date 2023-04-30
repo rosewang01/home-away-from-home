c7af964247220ca26f30fdadfec102523297ca29
Rose Wang <rosewang01@gmail.com>
2023-04-30 15:54:37 -0400

    commenting our errors

--- backend/src/controllers/state.controller.ts
+++ backend/src/controllers/state.controller.ts
@@ -19,39 +19,41 @@ const getBestCitiesByEmployer = async (req: Request, res: Response, next: NextFu
   res.status(200).json(cityData);
 };
 
-const getBestCitiesByGrowth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const { state } = req.params;
-  const cityData = await getAllCities(state);
-  cityData.sort((a, b) => b.average_housing_price_growth - a.average_housing_price_growth);
-  res.status(200).json(cityData);
-};
+// const getBestCitiesByGrowth = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//   const { state } = req.params;
+//   const cityData = await getAllCities(state);
+//   cityData.sort((a, b) => b.average_housing_price_growth - a.average_housing_price_growth);
+//   res.status(200).json(cityData);
+// };
 
-const getBestCitiesByDebt = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const { state } = req.params;
-  const cityData = await getAllCities(state);
-  cityData.sort((a, b) => (b.average_housing_price / b.average_salary) - (a.average_housing_price / a.average_salary));
-  res.status(200).json(cityData);
-};
+// const getBestCitiesByDebt = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//   const { state } = req.params;
+//   const cityData = await getAllCities(state);
+//   cityData.sort((a, b) => (b.average_housing_price / b.average_salary) - (a.average_housing_price / a.average_salary));
+//   res.status(200).json(cityData);
+// };
 
-const getBestCitiesByH1bVolume = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const { state } = req.params;
-  const cityData = await getAllCities(state);
-  cityData.sort((a, b) => b.h1b_volume - a.h1b_volume);
-  res.status(200).json(cityData);
-}
+// const getBestCitiesByH1bVolume = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//   const { state } = req.params;
+//   const cityData = await getAllCities(state);
+//   cityData.sort((a, b) => b.h1b_volume - a.h1b_volume);
+//   res.status(200).json(cityData);
+// }
 
-const getBestCitiesByH1bSuccessRate = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  const { state } = req.params;
-  const cityData = await getAllCities(state);
-  cityData.sort((a, b) => b.h1b_success_rate - a.h1b_success_rate);
-  res.status(200).json(cityData);
-}
+// const getBestCitiesByH1bSuccessRate = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//   const { state } = req.params;
+//   const cityData = await getAllCities(state);
+//   cityData.sort((a, b) => b.h1b_success_rate - a.h1b_success_rate);
+//   res.status(200).json(cityData);
+// }
 
 export {
   getAllCityDataByState,
   getBestCitiesByJob,
   getBestCitiesByEmployer,
-  getBestCitiesByGrowth,
-  getBestCitiesByDebt,
+  // getBestCitiesByGrowth,
+  // getBestCitiesByDebt,
+  // getBestCitiesByH1bVolume,
+  // getBestCitiesByH1bSuccessRate
 };
 
--- backend/src/routes/state.route.ts
+++ backend/src/routes/state.route.ts
@@ -1,12 +1,18 @@
 import express from "express";
-import { getAllCityDataByState, getBestCitiesByDebt, getBestCitiesByEmployer, getBestCitiesByGrowth, getBestCitiesByJob } from "../controllers/state.controller.js";
+import { 
+    getAllCityDataByState, 
+    // getBestCitiesByDebt, 
+    getBestCitiesByEmployer, 
+    // getBestCitiesByGrowth, 
+    getBestCitiesByJob
+} from "../controllers/state.controller.js";
 
 const stateRouter = express.Router();
 
 stateRouter.get("/all", getAllCityDataByState);
 stateRouter.get("/job/:job", getBestCitiesByJob);
 stateRouter.get("/employer/:employer", getBestCitiesByEmployer);
-stateRouter.get("/growth", getBestCitiesByGrowth);
-stateRouter.get("/debt", getBestCitiesByDebt);
+// stateRouter.get("/growth", getBestCitiesByGrowth);
+// stateRouter.get("/debt", getBestCitiesByDebt);
 
 export { stateRouter };
