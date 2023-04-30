5446811c35855b8b0d31884f4d58a53d076822d7
Rose Wang <rosewang01@gmail.com>
2023-04-30 09:41:42 -0400

    finished zipcode routes

--- backend/src/models/zipcode.model.ts
+++ backend/src/models/zipcode.model.ts
@@ -1,13 +1,8 @@
 interface IZipCode {
     zip_code: string;
-<<<<<<< HEAD
     
     average_housing_price: number;
     average_housing_market_growth: number;
-=======
-    metro_region: string;
-    metro_region_state_code: string;
->>>>>>> 1117913 (models)
 }
 
 export default IZipCode;
--- backend/src/services/city.service.ts
+++ backend/src/services/city.service.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {redisGet, redisSet} from "../utils/redis.js";
 import type ICity from "../models/city.model.js";
 import {sqlQuery} from "../utils/sql.js";
@@ -192,3 +193,64 @@ export {
   getCitiesWithJobFilter,
   getCitiesWithEmployerFilter,
 }
+=======
+import { sqlQuery } from "../utils/sql.js";
+import type IZipCode from "../models/zipcode.model.js";
+
+const getAllZipCodes = async (city_name: string): Promise<IZipCode[]> => {
+    const zipsRaw = await sqlQuery(`
+        WITH mr AS (SELECT metro_region, metro_region_state_code
+            FROM city
+            WHERE city_name = ${city_name}),
+        zips AS (SELECT zip_code
+            FROM mr JOIN zip_code zc
+                on mr.metro_region = zc.metro_region
+                        AND mr.metro_region_state_code = zc.metro_region_state_code)
+        SELECT h.zip_code, new_listings_yoy, median_sale_price
+        FROM housing_data h JOIN zips ON zips.zip_code = h.zip_code
+        GROUP BY h.zip_code;
+    `);
+    return zipsRaw as IZipCode[];
+};
+
+const getBestGrowthZipCodes = async (city_name: string): Promise<IZipCode[]> => {
+    const zipsRaw = await sqlQuery(`
+        WITH mr AS (SELECT metro_region, metro_region_state_code
+            FROM city
+            WHERE city_name = ${city_name}),
+        zips AS (SELECT zip_code
+            FROM mr JOIN zip_code zc
+                on mr.metro_region = zc.metro_region
+                        AND mr.metro_region_state_code = zc.metro_region_state_code),
+        housing AS (SELECT h.zip_code, new_listings_yoy, median_sale_price
+                FROM housing_data h JOIN zips ON zips.zip_code = h.zip_code)
+        SELECT zip_code, new_listings_yoy, median_sale_price
+        FROM housing
+        ORDER BY new_listings_yoy DESC
+        LIMIT 5;
+    `);
+    return zipsRaw as IZipCode[];
+}
+
+const getBestCostZipCodes = async (city_name: string): Promise<IZipCode[]> => {
+    const zipsRaw = await sqlQuery(`
+        WITH mr AS (SELECT metro_region, metro_region_state_code
+            FROM city
+            WHERE city_name = ${city_name}),
+        zips AS (SELECT zip_code
+            FROM mr JOIN zip_code zc
+                on mr.metro_region = zc.metro_region
+                        AND mr.metro_region_state_code = zc.metro_region_state_code),
+        housing AS (SELECT h.zip_code, new_listings_yoy, median_sale_price
+                FROM housing_data h JOIN zips ON zips.zip_code = h.zip_code)
+        SELECT zip_code, new_listings_yoy, median_sale_price
+        FROM housing
+        ORDER BY median_sale_price ASC
+        LIMIT 5;
+    `);
+    return zipsRaw as IZipCode[];
+}
+
+
+export { getAllZipCodes, getBestGrowthZipCodes, getBestCostZipCodes };
+>>>>>>> af174c8 (finished zipcode routes)
