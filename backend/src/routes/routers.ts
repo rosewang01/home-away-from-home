403acf0f84a56c0392d7171f2ed0f723797797d7
Vincent Tiu <vincetiu8@gmail.com>
2023-05-02 15:25:27 -0400

    add other routers

--- backend/src/routes/routers.ts
+++ backend/src/routes/routers.ts
@@ -2,6 +2,8 @@ import { type Router } from "express";
 import { stateRouter } from "./state.route.js";
 import { countryRouter } from "./country.route.js";
 import {cityRouter} from "./city.route.js";
+import {jobRouter} from "./job.route.js";
+import {employerRouter} from "./employer.route.js";
 
 const routers: Array<{ prefix: string; router: Router }> = [
   {
@@ -15,6 +17,14 @@ const routers: Array<{ prefix: string; router: Router }> = [
   {
     prefix: "/api/city/:city_name",
     router: cityRouter,
+  },
+  {
+    prefix: "/api/jobs",
+    router: jobRouter,
+  },
+  {
+    prefix: "/api/employers",
+    router: employerRouter
   }
 ];
 
