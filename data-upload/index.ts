10b7acfe19a1efb1b3776694fdbe547bc37f6697
Vincent Tiu <vincetiu8@gmail.com>
2023-05-02 20:48:21 -0400

    we upload some data

--- data-upload/index.ts
+++ data-upload/index.ts
@@ -33,169 +33,169 @@ console.log("database created");
 await connection.query("USE home_away_from_home");
 console.log("database selected");
 
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS state
-(
-    state_code VARCHAR(2) NOT NULL,
-    state_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (state_code),
-    UNIQUE (state_name)
-)
-  `
-);
-console.log("table state created");
-
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS metro_region (
-    region_name VARCHAR(255) NOT NULL,
-    state_code VARCHAR(2) NOT NULL,
-    PRIMARY KEY (state_code, region_name),
-    FOREIGN KEY (state_code) REFERENCES state(state_code) ON DELETE CASCADE
-)
-    `
-);
-console.log("table metro_region created");
-
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS city (
-    city_name VARCHAR(255) NOT NULL,
-    state_code VARCHAR(2) NOT NULL,
-    metro_region VARCHAR(255) NOT NULL,
-    metro_region_state_code VARCHAR(2) NOT NULL,
-    PRIMARY KEY (state_code, city_name),
-    FOREIGN KEY (state_code) REFERENCES state(state_code) ON DELETE CASCADE,
-    FOREIGN KEY (metro_region_state_code, metro_region) REFERENCES metro_region(state_code, region_name) ON DELETE CASCADE
-)
-    `
-);
-console.log("table city created");
-
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS h1b_case (
-    case_id INT NOT NULL AUTO_INCREMENT,
-    case_status VARCHAR(2) NOT NULL,
-    emp_name VARCHAR(255) NOT NULL,
-    job_title VARCHAR(255) NOT NULL,
-    soc_code VARCHAR(20) NOT NULL,
-    soc_name VARCHAR(255) NOT NULL,
-    full_time_position TINYINT(1) NOT NULL,
-    prevailing_yearly_wage DECIMAL(10, 2) NOT NULL,
-    work_state VARCHAR(2) NOT NULL,
-    work_city VARCHAR(255) NOT NULL,
-    PRIMARY KEY (case_id),
-    FOREIGN KEY (work_state, work_city) REFERENCES city(state_code, city_name) ON DELETE CASCADE
-)
-    `
-);
-console.log("table h1b_case created");
-
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS zip_code (
-    zip_code INT NOT NULL,
-    metro_region VARCHAR(255) NOT NULL,
-    metro_region_state_code VARCHAR(2) NOT NULL,
-    PRIMARY KEY (zip_code),
-    FOREIGN KEY (metro_region_state_code, metro_region) REFERENCES metro_region(state_code, region_name) ON DELETE CASCADE
-)
-    `
-);
-console.log("table zip_code created");
-
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS housing_data (
-    zip_code INT NOT NULL,
-    property_type VARCHAR(255) NOT NULL,
-    period_begin DATE NOT NULL,
-    period_end DATE NOT NULL,
-    median_sale_price DECIMAL(10, 2) NOT NULL,
-    median_sale_price_yoy DECIMAL(10, 9) NOT NULL,
-    median_list_price DECIMAL(10, 2) NOT NULL,
-    median_list_price_yoy DECIMAL(10, 9) NOT NULL,
-    median_ppsf DECIMAL(10, 2) NOT NULL,
-    median_ppsf_yoy DECIMAL(10, 9) NOT NULL,
-    median_list_ppsf DECIMAL(10, 2) NOT NULL,
-    median_list_ppsf_yoy DECIMAL(10, 9) NOT NULL,
-    homes_sold INT NOT NULL,
-    homes_sold_yoy DECIMAL(10, 9) NOT NULL,
-    new_listings INT NOT NULL,
-    new_listings_yoy DECIMAL(10, 9) NOT NULL,
-    inventory INT NOT NULL,
-    inventory_yoy DECIMAL(10, 9) NOT NULL,
-    median_dom DECIMAL(10, 2) NOT NULL,
-    median_dom_yoy DECIMAL(10, 9) NOT NULL,
-    avg_sale_to_list DECIMAL(10, 9) NOT NULL,
-    avg_sale_to_list_yoy DECIMAL(10, 9) NOT NULL,
-    PRIMARY KEY (zip_code, property_type, period_begin, period_end),
-    FOREIGN KEY (zip_code) REFERENCES zip_code(zip_code) ON DELETE CASCADE
-)
-    `
-);
-console.log("table housing_data created");
-
-await connection.query(
-  `
-INSERT IGNORE INTO state (state_code, state_name) VALUES
-    ('AL', 'Alabama'),
-    ('AK', 'Alaska'),
-    ('AZ', 'Arizona'),
-    ('AR', 'Arkansas'),
-    ('CA', 'California'),
-    ('CO', 'Colorado'),
-    ('CT', 'Connecticut'),
-    ('DE', 'Delaware'),
-    ('DC', 'District of Columbia'),
-    ('FL', 'Florida'),
-    ('GA', 'Georgia'),
-    ('HI', 'Hawaii'),
-    ('ID', 'Idaho'),
-    ('IL', 'Illinois'),
-    ('IN', 'Indiana'),
-    ('IA', 'Iowa'),
-    ('KS', 'Kansas'),
-    ('KY', 'Kentucky'),
-    ('LA', 'Louisiana'),
-    ('ME', 'Maine'),
-    ('MD', 'Maryland'),
-    ('MA', 'Massachusetts'),
-    ('MI', 'Michigan'),
-    ('MN', 'Minnesota'),
-    ('MS', 'Mississippi'),
-    ('MO', 'Missouri'),
-    ('MT', 'Montana'),
-    ('NE', 'Nebraska'),
-    ('NV', 'Nevada'),
-    ('NH', 'New Hampshire'),
-    ('NJ', 'New Jersey'),
-    ('NM', 'New Mexico'),
-    ('NY', 'New York'),
-    ('NC', 'North Carolina'),
-    ('ND', 'North Dakota'),
-    ('OH', 'Ohio'),
-    ('OK', 'Oklahoma'),
-    ('OR', 'Oregon'),
-    ('PA', 'Pennsylvania'),
-    ('RI', 'Rhode Island'),
-    ('SC', 'South Carolina'),
-    ('SD', 'South Dakota'),
-    ('TN', 'Tennessee'),
-    ('TX', 'Texas'),
-    ('UT', 'Utah'),
-    ('VT', 'Vermont'),
-    ('VA', 'Virginia'),
-    ('WA', 'Washington'),
-    ('WV', 'West Virginia'),
-    ('WI', 'Wisconsin'),
-    ('WY', 'Wyoming')
-    `
-);
-console.log("table state populated");
-
+// await connection.query(
+//   `
+// CREATE TABLE IF NOT EXISTS state
+// (
+//     state_code VARCHAR(2) NOT NULL,
+//     state_name VARCHAR(255) NOT NULL,
+//     PRIMARY KEY (state_code),
+//     UNIQUE (state_name)
+// )
+//   `
+// );
+// console.log("table state created");
+//
+// await connection.query(
+//   `
+// CREATE TABLE IF NOT EXISTS metro_region (
+//     region_name VARCHAR(255) NOT NULL,
+//     state_code VARCHAR(2) NOT NULL,
+//     PRIMARY KEY (state_code, region_name),
+//     FOREIGN KEY (state_code) REFERENCES state(state_code) ON DELETE CASCADE
+// )
+//     `
+// );
+// console.log("table metro_region created");
+//
+// await connection.query(
+//   `
+// CREATE TABLE IF NOT EXISTS city (
+//     city_name VARCHAR(255) NOT NULL,
+//     state_code VARCHAR(2) NOT NULL,
+//     metro_region VARCHAR(255) NOT NULL,
+//     metro_region_state_code VARCHAR(2) NOT NULL,
+//     PRIMARY KEY (state_code, city_name),
+//     FOREIGN KEY (state_code) REFERENCES state(state_code) ON DELETE CASCADE,
+//     FOREIGN KEY (metro_region_state_code, metro_region) REFERENCES metro_region(state_code, region_name) ON DELETE CASCADE
+// )
+//     `
+// );
+// console.log("table city created");
+//
+// await connection.query(
+//   `
+// CREATE TABLE IF NOT EXISTS h1b_case (
+//     case_id INT NOT NULL AUTO_INCREMENT,
+//     case_status VARCHAR(2) NOT NULL,
+//     emp_name VARCHAR(255) NOT NULL,
+//     job_title VARCHAR(255) NOT NULL,
+//     soc_code VARCHAR(20) NOT NULL,
+//     soc_name VARCHAR(255) NOT NULL,
+//     full_time_position TINYINT(1) NOT NULL,
+//     prevailing_yearly_wage DECIMAL(10, 2) NOT NULL,
+//     work_state VARCHAR(2) NOT NULL,
+//     work_city VARCHAR(255) NOT NULL,
+//     PRIMARY KEY (case_id),
+//     FOREIGN KEY (work_state, work_city) REFERENCES city(state_code, city_name) ON DELETE CASCADE
+// )
+//     `
+// );
+// console.log("table h1b_case created");
+//
+// await connection.query(
+//   `
+// CREATE TABLE IF NOT EXISTS zip_code (
+//     zip_code INT NOT NULL,
+//     metro_region VARCHAR(255) NOT NULL,
+//     metro_region_state_code VARCHAR(2) NOT NULL,
+//     PRIMARY KEY (zip_code),
+//     FOREIGN KEY (metro_region_state_code, metro_region) REFERENCES metro_region(state_code, region_name) ON DELETE CASCADE
+// )
+//     `
+// );
+// console.log("table zip_code created");
+//
+// await connection.query(
+//   `
+// CREATE TABLE IF NOT EXISTS housing_data (
+//     zip_code INT NOT NULL,
+//     property_type VARCHAR(255) NOT NULL,
+//     period_begin DATE NOT NULL,
+//     period_end DATE NOT NULL,
+//     median_sale_price DECIMAL(10, 2) NOT NULL,
+//     median_sale_price_yoy DECIMAL(10, 9) NOT NULL,
+//     median_list_price DECIMAL(10, 2) NOT NULL,
+//     median_list_price_yoy DECIMAL(10, 9) NOT NULL,
+//     median_ppsf DECIMAL(10, 2) NOT NULL,
+//     median_ppsf_yoy DECIMAL(10, 9) NOT NULL,
+//     median_list_ppsf DECIMAL(10, 2) NOT NULL,
+//     median_list_ppsf_yoy DECIMAL(10, 9) NOT NULL,
+//     homes_sold INT NOT NULL,
+//     homes_sold_yoy DECIMAL(10, 9) NOT NULL,
+//     new_listings INT NOT NULL,
+//     new_listings_yoy DECIMAL(10, 9) NOT NULL,
+//     inventory INT NOT NULL,
+//     inventory_yoy DECIMAL(10, 9) NOT NULL,
+//     median_dom DECIMAL(10, 2) NOT NULL,
+//     median_dom_yoy DECIMAL(10, 9) NOT NULL,
+//     avg_sale_to_list DECIMAL(10, 9) NOT NULL,
+//     avg_sale_to_list_yoy DECIMAL(10, 9) NOT NULL,
+//     PRIMARY KEY (zip_code, property_type, period_begin, period_end),
+//     FOREIGN KEY (zip_code) REFERENCES zip_code(zip_code) ON DELETE CASCADE
+// )
+//     `
+// );
+// console.log("table housing_data created");
+//
+// await connection.query(
+//   `
+// INSERT IGNORE INTO state (state_code, state_name) VALUES
+//     ('AL', 'Alabama'),
+//     ('AK', 'Alaska'),
+//     ('AZ', 'Arizona'),
+//     ('AR', 'Arkansas'),
+//     ('CA', 'California'),
+//     ('CO', 'Colorado'),
+//     ('CT', 'Connecticut'),
+//     ('DE', 'Delaware'),
+//     ('DC', 'District of Columbia'),
+//     ('FL', 'Florida'),
+//     ('GA', 'Georgia'),
+//     ('HI', 'Hawaii'),
+//     ('ID', 'Idaho'),
+//     ('IL', 'Illinois'),
+//     ('IN', 'Indiana'),
+//     ('IA', 'Iowa'),
+//     ('KS', 'Kansas'),
+//     ('KY', 'Kentucky'),
+//     ('LA', 'Louisiana'),
+//     ('ME', 'Maine'),
+//     ('MD', 'Maryland'),
+//     ('MA', 'Massachusetts'),
+//     ('MI', 'Michigan'),
+//     ('MN', 'Minnesota'),
+//     ('MS', 'Mississippi'),
+//     ('MO', 'Missouri'),
+//     ('MT', 'Montana'),
+//     ('NE', 'Nebraska'),
+//     ('NV', 'Nevada'),
+//     ('NH', 'New Hampshire'),
+//     ('NJ', 'New Jersey'),
+//     ('NM', 'New Mexico'),
+//     ('NY', 'New York'),
+//     ('NC', 'North Carolina'),
+//     ('ND', 'North Dakota'),
+//     ('OH', 'Ohio'),
+//     ('OK', 'Oklahoma'),
+//     ('OR', 'Oregon'),
+//     ('PA', 'Pennsylvania'),
+//     ('RI', 'Rhode Island'),
+//     ('SC', 'South Carolina'),
+//     ('SD', 'South Dakota'),
+//     ('TN', 'Tennessee'),
+//     ('TX', 'Texas'),
+//     ('UT', 'Utah'),
+//     ('VT', 'Vermont'),
+//     ('VA', 'Virginia'),
+//     ('WA', 'Washington'),
+//     ('WV', 'West Virginia'),
+//     ('WI', 'Wisconsin'),
+//     ('WY', 'Wyoming')
+//     `
+// );
+// console.log("table state populated");
+//
 // const metroRegions: [{ parent_metro_region: string }] = (await connection.query(
 //   `
 // SELECT DISTINCT parent_metro_region FROM raw_housing_data
@@ -216,12 +216,12 @@ console.log("table state populated");
 //         `
 //   );
 // }
-
+//
 // const parentMetroRegions = (await connection.query(
 //   `
 // SELECT DISTINCT region, parent_metro_region FROM raw_housing_data`
 // )) as [{ region: string; parent_metro_region: string }];
-
+//
 // await connection.query(
 //   `
 // INSERT IGNORE INTO zip_code (zip_code, metro_region, metro_region_state_code) VALUES ${parentMetroRegions
@@ -240,8 +240,8 @@ console.log("table state populated");
 //     .join(", ")}
 //         `
 // );
-
-console.log("table zip_code populated");
+//
+// console.log("table zip_code populated");
 
 // const cities = (await connection.query(
 //   `
@@ -270,9 +270,9 @@ console.log("table zip_code populated");
 //               .join(", ")}
 //         `
 // );
-
-console.log("table city populated");
-
+//
+// console.log("table city populated");
+//
 function titleCase(str: string): string {
   return str
     .toLowerCase()
@@ -311,34 +311,35 @@ function mysqlEscape(str: string): string {
   });
 }
 
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS temp_h1b_data (
-  case_id INT AUTO_INCREMENT,
-  case_status VARCHAR(255),
-  emp_name VARCHAR(255),
-  job_title VARCHAR(255),
-  soc_code VARCHAR(20),
-  soc_name VARCHAR(255),
-  full_time_position VARCHAR(2),
-  prevailing_wage DECIMAL(10, 2),
-  pw_unit VARCHAR(255),
-  work_state VARCHAR(255),
-  work_city VARCHAR(255),
-  PRIMARY KEY (case_id)
-)
-  `
-);
-//
 // await connection.query(
 //   `
-// INSERT INTO h1b_data (case_status, emp_name, job_title, soc_code, soc_name, full_time_position, prevailing_wage, pw_unit, work_state, work_city)
-// SELECT case_status, emp_name, job_title, soc_code, soc_name, full_time_position, prevailing_wage, pw_unit, work_state, work_city
-// FROM raw_h1b_data
+// CREATE TABLE IF NOT EXISTS temp_h1b_data (
+//   case_id INT AUTO_INCREMENT,
+//   case_status VARCHAR(255),
+//   emp_name VARCHAR(255),
+//   job_title VARCHAR(255),
+//   soc_code VARCHAR(20),
+//   soc_name VARCHAR(255),
+//   full_time_position VARCHAR(2),
+//   prevailing_wage DECIMAL(10, 2),
+//   pw_unit VARCHAR(255),
+//   work_state VARCHAR(255),
+//   work_city VARCHAR(255),
+//   PRIMARY KEY (case_id)
+// )
 //   `
 // );
 
-for (let i = 100000; i < 3360810; i += 10000) {
+// await connection.query(
+//   `
+// INSERT INTO temp_h1b_data (case_status, emp_name, job_title, soc_code, soc_name, full_time_position, prevailing_wage, pw_unit, work_state, work_city)
+// SELECT case_status, emp_name, job_title, soc_code, soc_name, full_time_position, prevailing_wage, pw_unit, work_state, work_city
+// FROM raw_h1b_data
+// LIMIT 2053652
+//   `
+// );
+
+for (let i = 2060000; i < 2053652; i += 10000) {
   console.log(`starting ${i}`);
 
   const result = await connection.query(
@@ -408,38 +409,38 @@ VALUES ${data
 }
 
 console.log("h1b data populated");
-
-await connection.query(
-  `
-CREATE TABLE IF NOT EXISTS temp_housing_data (
-  id INT AUTO_INCREMENT,
-  region VARCHAR(255),
-  property_type VARCHAR(255),
-  period_begin VARCHAR(255),
-  period_end VARCHAR(255),
-  median_sale_price DECIMAL(10, 2),
-  median_sale_price_yoy DECIMAL(10, 9),
-  median_list_price DECIMAL(10, 2),
-  median_list_price_yoy DECIMAL(10, 9),
-  median_ppsf DECIMAL(10, 2),
-  median_ppsf_yoy DECIMAL(10, 9),
-  median_list_ppsf DECIMAL(10, 2),
-  median_list_ppsf_yoy DECIMAL(10, 9),
-  homes_sold INT,
-  homes_sold_yoy DECIMAL(10, 9),
-  new_listings INT,
-  new_listings_yoy DECIMAL(10, 9),
-  inventory INT,
-  inventory_yoy DECIMAL(10, 9),
-  median_dom decimal(10, 2),
-  median_dom_yoy DECIMAL(10, 9),
-  avg_sale_to_list DECIMAL(10, 9),
-  avg_sale_to_list_yoy DECIMAL(10, 9),
-  PRIMARY KEY (id)
-)
-  `
-);
-
+//
+// await connection.query(
+//   `
+// CREATE TABLE IF NOT EXISTS temp_housing_data (
+//   id INT AUTO_INCREMENT,
+//   region VARCHAR(255),
+//   property_type VARCHAR(255),
+//   period_begin VARCHAR(255),
+//   period_end VARCHAR(255),
+//   median_sale_price DECIMAL(10, 2),
+//   median_sale_price_yoy DECIMAL(10, 9),
+//   median_list_price DECIMAL(10, 2),
+//   median_list_price_yoy DECIMAL(10, 9),
+//   median_ppsf DECIMAL(10, 2),
+//   median_ppsf_yoy DECIMAL(10, 9),
+//   median_list_ppsf DECIMAL(10, 2),
+//   median_list_ppsf_yoy DECIMAL(10, 9),
+//   homes_sold INT,
+//   homes_sold_yoy DECIMAL(10, 9),
+//   new_listings INT,
+//   new_listings_yoy DECIMAL(10, 9),
+//   inventory INT,
+//   inventory_yoy DECIMAL(10, 9),
+//   median_dom decimal(10, 2),
+//   median_dom_yoy DECIMAL(10, 9),
+//   avg_sale_to_list DECIMAL(10, 9),
+//   avg_sale_to_list_yoy DECIMAL(10, 9),
+//   PRIMARY KEY (id)
+// )
+//   `
+// );
+//
 // await connection.query(
 //   `
 // INSERT INTO temp_housing_data (region, property_type, period_begin, period_end, median_sale_price, median_sale_price_yoy, median_list_price, median_list_price_yoy, median_ppsf, median_ppsf_yoy, median_list_ppsf, median_list_ppsf_yoy, homes_sold, homes_sold_yoy, new_listings, new_listings_yoy, inventory, inventory_yoy, median_dom, median_dom_yoy, avg_sale_to_list, avg_sale_to_list_yoy)
@@ -448,7 +449,7 @@ CREATE TABLE IF NOT EXISTS temp_housing_data (
 //   `
 // );
 
-for (let i = 100000; i < 5301382; i += 10000) {
+for (let i = 1000000; i < 5301382; i += 10000) {
   console.log(`starting ${i}`);
 
   const result = await connection.query(
